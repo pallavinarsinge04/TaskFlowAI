c1b8873969de95f07f9f7eae82586138abf44de6
Pallavi Narsinge <pallavinarsinge123@gmail.com>
2026-08-08 19:38:09 +0530

    Fix AI route controller import

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -1,14 +1,8 @@
 import express from "express";
-import {
-  chat,
-  vision,
-  getHistory,
-} from "../controllers/aiController.js";
+import { chat } from "../controllers/aiController.js";
 
 const router = express.Router();
 
 router.post("/chat", chat);
-router.post("/vision", vision);
-router.get("/history/:userId", getHistory);
 
 export default router;
